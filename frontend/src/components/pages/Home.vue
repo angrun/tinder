c35b5f86b2e8cb1e411c2eceb6e5650af96284b9
kapive <kapive@ttu.ee>
2019-02-14 10:18:44 +0200

    Website structure

---
+++ frontend/src/components/pages/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="main">
+    <h2>Home</h2>
+    <div class="menu">
+      <nav>
+        <router-link to="/home" class="link">Home</router-link>
+        <router-link to="/chat" class="link">Chat</router-link>
+        <router-link to="/profile" class="link">Profile</router-link>
+      </nav>
+      <router-view/>
+    </div>
+    <div class="users">
+      <div class="photo">
+        Here should be users photo.
+      </div>
+      <div class="description">
+        Here should be user's description.
+      </div>
+      <div class="buttons">
+        Here should be buttons like/dislike.
+      </div>
+    </div>
+    <div class="footer">
+      Here should be footer.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style src="../style/MainStyle.css" scoped>
+</style>
---
+++ frontend/src/components/pages/Profile.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="main">
+    <h2>Profile</h2>
+    <div class="menu">
+      <nav>
+        <router-link to="/home" class="link">Home</router-link>
+        <router-link to="/chat" class="link">Chat</router-link>
+        <router-link to="/profile" class="link">Profile</router-link>
+      </nav>
+      <router-view/>
+    </div>
+    <div class="myAccount">
+      Here should be users data.
+      <div>
+        <div class="photo">
+          Here should be users photo.
+        </div>
+        <div class="information">
+          Here should be users information.
+        </div>
+      </div>
+      <div class="button">
+        Here should be button for information change.
+      </div>
+    </div>
+    <div class="footer">
+      Here should be footer.
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile'
+}
+</script>
+
+<style src="../style/MainStyle.css" scoped>
+</style>
